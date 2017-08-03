b6112fe329afada6506355e417997f13b5326961
rajandhaliwal80 <rdhali36@my.centennialcollege.ca>
2017-08-03 18:36:42 -0400

    Basic Version Of UI BMI Calculator

--- Lab26_300926123/BMI Calculator.Designer.cs
+++ Lab26_300926123/BMI Calculator.Designer.cs
@@ -33,24 +33,176 @@ namespace Lab26_300926123
         /// </summary>
         private void InitializeComponent()
         {
+            this.BMITableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.Calcalution = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.BMITableLayout.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // BMITableLayout
+            // 
+            this.BMITableLayout.ColumnCount = 2;
+            this.BMITableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.BMITableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.BMITableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.BMITableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.BMITableLayout.Controls.Add(this.label1, 0, 1);
+            this.BMITableLayout.Controls.Add(this.textBox1, 2, 1);
+            this.BMITableLayout.Controls.Add(this.label2, 0, 2);
+            this.BMITableLayout.Controls.Add(this.textBox2, 2, 2);
+            this.BMITableLayout.Controls.Add(this.Calcalution, 2, 0);
+            this.BMITableLayout.Controls.Add(this.radioButton1, 0, 0);
+            this.BMITableLayout.Controls.Add(this.radioButton2, 1, 0);
+            this.BMITableLayout.Location = new System.Drawing.Point(1, 181);
+            this.BMITableLayout.Name = "BMITableLayout";
+            this.BMITableLayout.RowCount = 3;
+            this.BMITableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23.1405F));
+            this.BMITableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 44.09449F));
+            this.BMITableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.07087F));
+            this.BMITableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            this.BMITableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.BMITableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 54F));
+            this.BMITableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.BMITableLayout.Size = new System.Drawing.Size(300, 200);
+            this.BMITableLayout.TabIndex = 0;
+            this.BMITableLayout.Paint += new System.Windows.Forms.PaintEventHandler(this.ButtonTableLayout_Paint);
+            // 
+            // Result
+            // 
+            this.Result.BackColor = System.Drawing.Color.MistyRose;
+            this.Result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Result.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.Result.Enabled = false;
+            this.Result.Location = new System.Drawing.Point(4, 110);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            this.Result.Size = new System.Drawing.Size(294, 65);
+            this.Result.TabIndex = 1;
+            this.Result.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Calcalution
+            // 
+            this.Calcalution.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Calcalution.BackColor = System.Drawing.Color.IndianRed;
+            this.BMITableLayout.SetColumnSpan(this.Calcalution, 2);
+            this.Calcalution.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calcalution.Location = new System.Drawing.Point(3, 32);
+            this.Calcalution.Name = "Calcalution";
+            this.Calcalution.Size = new System.Drawing.Size(294, 50);
+            this.Calcalution.TabIndex = 0;
+            this.Calcalution.Text = "Calculate BMI";
+            this.Calcalution.UseVisualStyleBackColor = false;
+            this.Calcalution.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "My Height";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(153, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 34);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "My Weight";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(153, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 34);
+            this.textBox2.TabIndex = 7;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(144, 23);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Imperial";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(153, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(144, 23);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Metric";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Cornsilk;
             this.ClientSize = new System.Drawing.Size(302, 433);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.BMITableLayout);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BMI CAlculator";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.BMITableLayout.ResumeLayout(false);
+            this.BMITableLayout.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel BMITableLayout;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Button Calcalution;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
--- Lab26_300926123/BMI Calculator.cs
+++ Lab26_300926123/BMI Calculator.cs
@@ -26,5 +26,25 @@ namespace Lab26_300926123
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonTableLayout_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
